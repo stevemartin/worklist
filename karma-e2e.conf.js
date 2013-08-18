e250e94f5ea034153a4806dfbe195803f153507d
Steve Martin <steve.martin@fundingcircle.com>
2013-08-18 22:35:58 +0100

    karma modification

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,64 +1,67 @@
 // Karma E2E configuration
 
 module.exports = function(config){
-  config.set({});
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath : '',
+
+    frameworks : ["ng-scenario", "jasmine"],
+    // list of files / patterns to load in the browser
+    files : [
+      // 'vendor/assets/javascripts/angular-1.0.7/*.min.js',
+      'vendor/assets/javascripts/angular-1.0.7/angular.js',
+      'vendor/assets/javascripts/angular-1.0.7/angular-scenario.js',
+      'vendor/assets/javascripts/angular-1.0.7/angular-resource.js',
+      'vendor/assets/javascripts/angular-1.0.7/angular-mocks.js',
+      'app/assets/javascripts/**/*.js',
+      'spec/javascripts/e2e/**/*_spec.js'
+    ],
+
+    proxies : {
+      '/': 'http://0.0.0.0:8080/spec/javascripts/e2e/'
+    },
+
+    urlRoot : '/_karma_/',
+    // list of files to exclude
+    exclude : [],
+
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters : ['dots'],
+
+    // web server port
+    port : 8080,
+
+    // cli runner port
+    runnerPort : 8080,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors : true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    // logLevel = LOG_INFO;
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch : true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // browsers : ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout : 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun : false,
+
+  });
 
 }
-// base path, that will be used to resolve files and exclude
-basePath = '';
-
-frameworks = ["ng-scenario"];
-// list of files / patterns to load in the browser
-files = [
-  'vendor/assets/javascripts/angular/angular.js',
-  'vendor/assets/javascripts/angular-resource/angular-resource.js',
-  'vendor/assets/javascripts/angular-scenario/angular-scenario.js',
-  'app/assets/javascripts/*.js',
-  'app/assets/javascripts/*/.js',
-  'spec/javascripts/e2e/**/*_spec.js'
-];
-
-proxies = {
-  '/': 'http://localhost:8080/spec/javascripts/e2e/'
-};
-
-urlRoot = '/_karma_/';
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['dots'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 8080;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-// logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false; 
