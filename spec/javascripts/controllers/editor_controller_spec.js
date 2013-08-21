4d1ff81198aae5ea5f3b7456560fa6d44c42bbe0
Damien McMahon <damien.mcmahon@gmail.com>
2013-08-21 21:52:09 +0100

    Adds default text for fields as an attribute

--- spec/javascripts/controllers/editor_controller_spec.js
+++ spec/javascripts/controllers/editor_controller_spec.js
@@ -10,8 +10,14 @@ describe("controllers", function(){
     }));
 
     it("should initialise with scope editable false", function(){
-      expect($scope.editable).toBe(false);
+      expect($scope.editing).toBe(false);
     });
+
+    // it('should set set view menu to true when called', function(){
+    //   expect($scope.showMenu).toBe(false);
+    //   $scope.toggleMenu();
+    //   expect($scope.showMenu).toBe(true);
+    // });
   });
 
 });
