2dff9f54d0110bd2f9eb61d46ba8afb30ac9db62
Damien McMahon <damien.mcmahon@gmail.com>
2013-09-15 17:03:33 +0100

    More arch stuff

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -4,13 +4,10 @@
   var app = angular.module('worklist',
     ['worklist.directives','worklist.services']);
 
-  app.controller('EditCtrl', ['$scope','WorkList',function($scope, WorkList){
+  app.controller('AppCtrl', ['$scope','WorkList',function($scope, WorkList){
       $scope.editing = true;
-
-      $scope.saveWorkList = function() {
-	console.log($scope.worklist);
-	//$scope.workList.$save();
-      };
+      $scope.saveWorkList = function() {};
   }]);
+
 
 })();
