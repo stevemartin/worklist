77db19a47d585b51246c62f7c61c3b0e493f1568
Damien McMahon <damien.mcmahon@gmail.com>
2013-10-09 06:30:17 +0100

    Styling changes for job history, working towards repeatable sections

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -16,47 +16,37 @@
   app.controller('AppCtrl', ['$scope','WorkList',function($scope, WorkList){
   }]);
 
-  app.controller('EditCtrl', ['$scope', '$cookieStore', 'MyWorkList', 'WorkList',function($scope, $cookieStore, MyWorkList, WorkList){
+  app.controller('EditCtrl', ['$scope', '$cookieStore','MyWorkList', 'WorkList', function( $scope,$cookieStore, MyWorkList, WorkList ){
 
     // $scope.worklist = new WorkList( window.worklist_data );
 
-    var determineUrlState = function() {
-      var url = $cookieStore.get("url")
-
-      console.log( "CookieUrl: " + url )
+    function determineUrlState() {
+      var url = $cookieStore.get("url");
 
       if(typeof url === 'undefined'){
 
         if( typeof window.worklist_data.user_profile.url !== 'undefined'){
-
-          console.log( "Setting Url Cookie: " + url )
-          $cookieStore.put("url", window.worklist_data.user_profile.url)
+	  $cookieStore.put("url", window.worklist_data.user_profile.url);
           return $scope.worklist;
-
         } else {
-
-          console.log( "Getting default worklist: " + url )
           return new WorkList( window.worklist_data );
-
         }
       } else {
-
-          console.log( "Getting MY worklist: " + url )
         return MyWorkList.get({url:url});
-
       }
     }
+
+    //we're either returning a new resource or a promise (MyWorkList.get)
     $scope.worklist = determineUrlState();
 
-    console.log( $scope.worklist );
     $scope.saveWorkList = function() {
       if(typeof $scope.worklist.user_id === 'undefined'){
         $scope.worklist.$save(function(data){
-          $cookieStore.put("url", data.user_profile.url)
+	  $cookieStore.put("url", data.user_profile.url);
         });
       } else {
         $scope.worklist.$update(function(data){
-          $cookieStore.put("url", data.user_profile.url)
+	  $cookieStore.put("url", data.user_profile.url);
         });
       }
 
--- app/assets/javascripts/services/services.js
+++ app/assets/javascripts/services/services.js
@@ -4,25 +4,23 @@
   var services = angular.module('worklist.services',['ngResource']);
 
   services.factory('MyWorkList', ['$resource', function( $resource, type ){
-      return $resource('/me/:url',{},
-                       {
-                            get:{'method':'GET'},
-                           save:{'method':'POST'},
-                         update:{'method':'PUT'}
-                       }
-                       );
+      return $resource('/me/:url',{},{
+			get:{'method':'GET'},
+			save:{'method':'POST'},
+			update:{'method':'PUT'}
+		      });
   }]);
 
   services.factory('WorkList', ['$resource', function( $resource, type ){
     // return $resource('/users/create_profile');
-      function setUrlCookie(){
+      function setUrlCookie(WorkList){
+	console.log("WL: ", WorkList, arguments);
         console.log("Setting url cookie!!!!");
       }
-      return $resource('/users/profile',{},
-                       {save:{'method':'POST', callback:'setUrlCookie'},
-                       update:{'method':'PUT', callback:'setUrlCookie'}}
-                       );
-
+      return $resource('/users/profile',{},{
+			save: {'method':'POST', callback:'setUrlCookie'},
+			update:{'method':'PUT', callback:'setUrlCookie'}
+		      });
   }]);
 
 
