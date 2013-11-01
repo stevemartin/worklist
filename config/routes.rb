816a4051010b68a639729128b57a8ed495972d8d
Steve Martin <steve.martin@fundingcircle.com>
2013-11-01 23:46:08 +0000

    EUzaah Sineup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Worklist::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   post 'users/profile' => 'user/profiles#create'
   put 'users/profile' => 'user/profiles#update'
