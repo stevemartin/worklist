caf45be68d9a416064d8d292ef4d3ba587c21790
Martian Sleeves <martian.sleeves@gmail.com>
2014-04-12 18:53:37 +0100

    fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -128,7 +128,6 @@ namespace :deploy do
     end
   end
 
-  # after :restart, 'deploy:stop', 'deploy:start'
 
   after :finishing, 'deploy:cleanup'
 
@@ -137,36 +136,4 @@ end
 after 'deploy:updated', 'db:migrate'
 before 'deploy:updated', 'bundle:install'
 after 'bundle:install', 'deploy:assets:precompile'
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
-
-# namespace :deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-#   after :finishing, 'deploy:cleanup'
-
-# end
+after 'db:migrate', 'deploy:stop', 'deploy:start'
