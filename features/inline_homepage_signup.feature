db8f9e38a36e9d8ed76c5a1e95edcabeee2eb1a2
Martian Sleeves <martian.sleeves@gmail.com>
2014-04-23 20:58:59 +0100

    test persistence across reloads

--- features/inline_homepage_signup.feature
+++ features/inline_homepage_signup.feature
@@ -10,4 +10,5 @@ Feature: Inline homepage signup
     When I edit the homepage CV
     And I should be able to save it and register
     Then I should be logged in
+    And the worklist should be persisted when I reload the page
     And I should be able to sign out
--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -20,7 +20,7 @@ When(/^I edit the homepage CV$/) do
 end
 
 Then(/^I should be able to save it and register$/) do
-  page.should_not have_content "Sign Out"
+  page.should_not have_content "Sign out"
   click_button "Save"
   fill_in 'signin-email', :with => "bugs.bunny@testdomaintest.cam"
   fill_in 'signin-password', :with => "SomecoolPass123"
@@ -31,6 +31,11 @@ Then(/^I should be logged in$/) do
   # binding.pry
   page.should have_content "Sign Out"
   page.should_not have_content "Sign In"
+end
+
+Then(/^the worklist should be persisted when I reload the page$/) do
+  visit '/'
+  first('input').value.should eq("Bugs Bunny")
 end
 
 Then(/^I should be able to sign out$/) do
