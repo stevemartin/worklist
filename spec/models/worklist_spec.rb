89e5704396c18abec2a40cacb99801e239cba8a1
Martian Sleeves <martian.sleeves@gmail.com>
2014-05-08 08:15:05 +0100

    fix url generate test

--- spec/models/worklist_spec.rb
+++ spec/models/worklist_spec.rb
@@ -10,11 +10,10 @@ describe Worklist do
 
     before do
       SecureRandom.should_receive(:uuid).and_return(uuid)
-      subject.stub(:id).and_return(1)
     end
 
     xit 'should generate url composite' do
-      subject.url.should == 1.to_s(36)
+      subject.url.should == subject.id.to_s(36)
       subject.url_key.should == uuid
     end
 
