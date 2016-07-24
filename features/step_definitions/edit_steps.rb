64899bd4ab580bf3d22c4030766b151f16b1464e
tevio <steve+1@martian.media>
2016-07-24 19:11:23 +0100

    attempt to fix headless cucumber features

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -1,14 +1,17 @@
 When(/^I add another section and save it$/) do
-  container = first('.container')
-  container.click
+  # container = first('.container')
+  # container.click
+  find_button 'Add Job'
+  page.execute_script('var element = document.getElementById("add-job-button"); element.scrollIntoView();')
   click_button 'Add Job'
-  container = first('.container')
-  container.click
+  # container = first('.container')
+  # container.click
   all('section.job').count
   50.times do
     sleep 0.1 if ( all('section.job').count != 2)
   end
   all('section.job').count.should eq( 2 )
+  # binding.pry
   within(:xpath, all('section.job').last.path) { find('input.employer').set('Jimmy Joes') }
   click_button "Save"
 end
@@ -24,6 +27,7 @@ end
 
 Then(/^I should be able to remove it$/) do
   sleep(0.1)
+  # binding.pry
   within(:xpath, all('section.job').last.path) { first(:xpath, '//a[text()="Remove"]').click }
   click_button "Save"
   sleep(0.1)
